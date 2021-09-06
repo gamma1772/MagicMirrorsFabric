0d234892202d09eb2058d93b66124fc96fb33033
Marko Dujović <dujovicm@outlook.com>
2021-09-06 19:28:35 +0200

    Implement TeleportEvent (TODO)

---
+++ src/main/java/com/gamma1772/magicmirrors/api/util/teleport/TeleportEvent.java
@@ -0,0 +1,8 @@
+package com.gamma1772.magicmirrors.api.util.teleport;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+public interface TeleportEvent {
+    //TODO
+}
